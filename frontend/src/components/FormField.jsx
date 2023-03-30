5882916663ffc9085070915eac0a4f63b5d3ee29
Zhile Lin <zhlin04@gmail.com>
2023-03-30 14:23:11 -0700

    connected from field with onchange

--- frontend/src/components/FormField.jsx
+++ frontend/src/components/FormField.jsx
@@ -26,7 +26,7 @@ const FormField = ({labelName,type,name,placeholder,value,handleChange,isSurpris
          name={name}
          placeholder={placeholder}
          value={value}
-         onChnage={handleChange}
+         onChange={handleChange}
          required
          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
        />
--- frontend/utils/index.js
+++ frontend/utils/index.js
@@ -2,7 +2,7 @@ import {surpriseMePrompts} from '../constants'
 
 
 export function getRandomPrompt(prompt){
-        const randomIndex = Math.floor(Math.random() * surpriseMePrompts.legnth)
+        const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length)
         const randomPrompt = surpriseMePrompts[randomIndex]
 
         if(randomPrompt === prompt) return getRandomPrompt(prompt)
