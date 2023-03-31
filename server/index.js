08b93d3609a160577d3b7c2df47368d0b4df021e
Zhile Lin <zhlin04@gmail.com>
2023-03-31 16:26:55 -0700

    migrate backed to cloud

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const startserver = async()=>{
     try{ 
        connectDB(process.env.MONGODB_URL)
        app.listen(8080,()=>{
-       console.log('Server has started on port http://localhost:8080')
+       console.log('Server has started on port https://journizen.onrender.com')
     })
     }catch(error){
           console.log(error) 
